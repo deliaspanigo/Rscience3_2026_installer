f813d272a7f77da327695fbb2bb465fde2244e6e
unknown <david.elias949@gmail.com>
2026-03-18 17:21:21 +0100

    Upgrade to v.3.3.0: Windows installer.

--- script999_github.R
+++ script999_github.R
@@ -2,13 +2,13 @@
 git add .
 
 # 2. Crea el commit con un mensaje descriptivo
-git commit -m "Upgrade to v.3.2.6: Added professional VBS launcher, splash screen, port 1410 management and onStart at app.R"
+git commit -m "Upgrade to v.3.3.0: Windows installer."
 
 # 3. Sube los cambios a la rama principal (usualmente 'main' o 'master')
 git push origin main
 
 # 4. Crea una etiqueta anotada para la versión 3.2.6
-git tag -a v.3.2.6 -m "Port 1410 management and onStart at app.R"
+git tag -a v.3.3.0 -m Windows installer."
 
 # 5. Sube la etiqueta a GitHub
-git push origin v.3.2.6
+git push origin v.3.3.0
